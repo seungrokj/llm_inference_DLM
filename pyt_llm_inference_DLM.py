8c7c8c7061827f2aefe536a4bbd3b878771c3922
seungrokjung <seungrok.jung@amd.com>
2024-03-08 05:09:16 +0000

    update

--- pyt_llm_inference_DLM.py
+++ pyt_llm_inference_DLM.py
@@ -122,7 +122,7 @@ def main():
                 model = T5ForConditionalGeneration.from_pretrained(args.model_path, torch_dtype=dtype, trust_remote_code=True, device_map="auto")
             elif args.model_path == "tiiuae/falcon-7b":
                 dtype=torch.bfloat16
-                model = AutoModelForCausalLM.from_pretrained(args.model_path, attn_implementation=args.attn_implementation, torch_dtype=dtype, trust_remote_code=True, device_map="auto")
+                model = AutoModelForCausalLM.from_pretrained(args.model_path, torch_dtype=dtype, trust_remote_code=True, device_map="auto")
             else:
                 model = AutoModelForCausalLM.from_pretrained(args.model_path, torch_dtype=dtype, trust_remote_code=True, device_map="auto")
     else: 
